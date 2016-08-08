46e77d424802e781dfdf15e81e487ce9a2b6fab6
rashmi <rashmi@Rashmis-MacBook-Pro.local>
2016-08-07 21:37:31 -0700

    adding next and previous buttons

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -21,33 +21,52 @@
   }
 }());
 },{"./lightbox.controller":2,"./utility.service":3}],2:[function(require,module,exports){
-var LightBoxController = function(){
+var LightBoxController = function(utility){
   'use strict';
   var lightBoxBg = document.getElementById('lgbg');
   var lightBox = document.getElementById('lg');
   var lightBoxImage = document.getElementById('lgImage');
+  var body = document.body;
+  var images = document.getElementsByClassName('thumbnail');
+  var totalImages = images.length;
+  var prev = document.getElementById('prev');
+  var next = document.getElementById('next');
+
 
 function dimissLightBox (){
-    lightBoxBg.style.display = 'none';
-    lightBox.style.display ='none';  
-}
-function startLightBox(image){
-
-    console.log(lightBox);
-    var source = image.target.src;
-    console.log(source);
-    lightBoxImage.src = source;  
+  if(utility.hasClass(body,'no-scroll')){
+    utility.removeClass(body,'no-scroll');
+  }
+  lightBoxBg.style.display = 'none';
+  lightBox.style.display ='none';
+  }
+function startLightBox(i,image){
+  console.log(i);
+  var source = image.target.src;
+  lightBoxImage.src = source;
+  if(!utility.hasClass(body,'no-scroll')){
+    body.className += "no-scroll";
+  }
+  if(i === 0){
+    prev.style.display = 'none';
+  }
+  else if (i === (totalImages-1)){
+    next.style.display ='none';
+  }
+  else {
+    prev.style.display = 'inline';
+    next.style.display ='inline';
+  }
     lightBoxBg.style.display = 'block';
     lightBox.style.display ='block';
   } 
 
   this.startListener = function startListener(){
-    var images = document.getElementsByClassName('thumbnail');
-    //  console.log(images);
+
     for (var i = 0; i < images.length; i++){
-      images[i].addEventListener('click', startLightBox.bind(this));
+      images[i].addEventListener('click', startLightBox.bind(this,i));
     } 
-//to be removed
+
     lightBoxBg.addEventListener('click',dimissLightBox);   
   };  
   
@@ -61,6 +80,13 @@ module.exports = LightBoxController;
 var Utility = function(){
   'use strict';
 
+this.hasClass = function hasClass(element,cls) {
+  return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+};
+
+this.removeClass =function(element,cls){
+  element.classList.remove(cls);
+};
 };
 
 module.exports = Utility;
