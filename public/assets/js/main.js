47c2c82d1ef75a11be4f3bcaa9c8ad5b06231662
rashmi <rashmi@Rashmis-MacBook-Pro.local>
2016-08-08 00:55:03 -0700

    next and previous navigation

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -31,6 +31,7 @@ var LightBoxController = function(utility){
   var totalImages = images.length;
   var prev = document.getElementById('prev');
   var next = document.getElementById('next');
+  var currentImageId = 0;
 
 
 function dimissLightBox (){
@@ -40,26 +41,47 @@ function dimissLightBox (){
   lightBoxBg.style.display = 'none';
   lightBox.style.display ='none';
   }
-function startLightBox(i,image){
-  console.log(i);
+function startLightBox(id,image){
   var source = image.target.src;
+
+  currentImageId = id;
   lightBoxImage.src = source;
   if(!utility.hasClass(body,'no-scroll')){
     body.className += "no-scroll";
   }
-  if(i === 0){
+  if(id === 0){
     prev.style.display = 'none';
-  }
-  else if (i === (totalImages-1)){
+  }  
+  else if (id === (totalImages-1)){
     next.style.display ='none';
-  }
-  else {
+  }  
+  else {  
     prev.style.display = 'inline';
-    next.style.display ='inline';
+    next.style.display ='inline';    
   }
     lightBoxBg.style.display = 'block';
     lightBox.style.display ='block';
-  } 
+  }
+
+
+function changeLightBoxElement(id){
+  currentImageId = id;
+  var image = images[id].getElementsByTagName('img')[0];
+  //console.log(image);
+  lightBoxImage.src = image.src;
+   if(id === 0){
+    prev.style.display = 'none';
+  }  
+  else if (id === (totalImages-1)){
+    next.style.display ='none';
+  }  
+  else {  
+    prev.style.display = 'inline';
+    next.style.display ='inline';    
+  }
+
+} 
+
 
   this.startListener = function startListener(){
 
@@ -67,8 +89,18 @@ function startLightBox(i,image){
       images[i].addEventListener('click', startLightBox.bind(this,i));
     } 
 
-    lightBoxBg.addEventListener('click',dimissLightBox);   
-  };  
+    lightBoxBg.addEventListener('click',dimissLightBox);
+     prev.addEventListener('click',function(){
+       if(currentImageId > 0){
+         changeLightBoxElement((currentImageId-1));
+       }
+     });
+     next.addEventListener('click',function(){
+       if(currentImageId < totalImages){
+         changeLightBoxElement((currentImageId+1));
+       }
+     });
+  };    
   
 
 
